338f26550967f929bcab68811a2d2b74798f90b3
Asif <asifansari94@gmail.com>
2026-02-05 16:50:48 +0530

    Removed Unused Code from MainActivity

--- app/src/main/java/com/berlinclockapp/presentation/MainActivity.kt
+++ app/src/main/java/com/berlinclockapp/presentation/MainActivity.kt
@@ -4,43 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.berlinclockapp.presentation.theme.ui.BerlinClockAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _root_ide_package_.com.berlinclockapp.presentation.theme.ui.BerlinClockAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            BerlinClockAppTheme {
+                App()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun App() {}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    _root_ide_package_.com.berlinclockapp.presentation.theme.ui.BerlinClockAppTheme {
-        Greeting("Android")
-    }
+    BerlinClockAppTheme {}
 }
