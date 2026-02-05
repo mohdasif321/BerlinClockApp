fd9c49e1fd027f047c42b5182e732ec32f29d300
Asif <asifansari94@gmail.com>
2026-02-05 19:23:47 +0530

    Added Test to check initial value for BerlinClockUIState for Bottom Minute light in view model

--- app/src/test/java/com/berlinclockapp/presentation/viewmodel/BerlinClockViewModelTest.kt
+++ app/src/test/java/com/berlinclockapp/presentation/viewmodel/BerlinClockViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.berlinclockapp.presentation.viewmodel
 
+import com.berlinclockapp.constants.BOTTOM_MINUTE_LIGHT_COUNT
 import com.berlinclockapp.constants.HOUR_LIGHT_COUNT
 import com.berlinclockapp.constants.INITIAL_TIME
 import com.berlinclockapp.constants.TOP_MINUTE_LIGHT_COUNT
@@ -36,6 +37,13 @@ class BerlinClockViewModelTest {
     fun `check topMinuteLight are initially OFF`() {
         Assert.assertTrue(berlinClockViewModel.berlinClockState.value.topMinuteLight == List(
             TOP_MINUTE_LIGHT_COUNT
+        ) { LightColor.OFF })
+    }
+
+    @Test
+    fun `check bottomMinuteLight are initially OFF`() {
+        Assert.assertTrue(berlinClockViewModel.berlinClockState.value.bottomMinuteLight == List(
+            BOTTOM_MINUTE_LIGHT_COUNT
         ) { LightColor.OFF })
     }
 
